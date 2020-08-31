37ed3738f0714376b520f5a4311c4bd4955b6d03
Hemanshu Varma <hemanshuvarma916@gmail.com>
2020-08-31 22:42:32 +0530

    Program2-MutableVariables

---
+++ P2-MutableVariables.kt
@@ -0,0 +1,14 @@
+//Execution starts from Main
+fun main(){
+
+    // "var" is a Mutable datatype, meaning it's value can be overwritten later
+    var myName = "Hemanshu"
+
+    // "println" prints the message and followed by a line separator
+    println(myName)
+
+    myName = "Hemanshu Varma"
+
+    // "print" prints only the message
+    print(myName)
+}
