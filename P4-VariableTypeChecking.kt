e115e421d507c18bb79fa0d9d79488364b8dc0d1
Hemanshu Varma <hemanshuvarma916@gmail.com>
2020-08-31 23:03:47 +0530

    Add: Program4-VariableTypeChecking

---
+++ P4-VariableTypeChecking.kt
@@ -0,0 +1,10 @@
+//Execution starts from Main
+fun main(){
+
+    val myName = "Hemanshu Varma"
+    val age = 5
+
+    //Prints type of variable, i.e., string, int, float etc
+    println(myName::class)
+    println(age::class)
+}
