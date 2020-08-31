62243a910db1ef2c009cb2aeafa68a8fe9ea664a
Hemanshu Varma <hemanshuvarma916@gmail.com>
2020-08-31 22:45:31 +0530

    Program3-ImmutableVariables

---
+++ P3-ImmutableVariables.kt
@@ -0,0 +1,12 @@
+//Execution starts from Main
+fun main(){
+
+    // "val" is a Immutable datatype, meaning it's value can't be overwritten later
+    val myName = "Hemanshu Varma"
+    val age = 5
+
+    println(myName) //Prints Hemanshu Varma
+    println(age) //Prints 5
+
+    //Reassigning throws compilation error
+}
