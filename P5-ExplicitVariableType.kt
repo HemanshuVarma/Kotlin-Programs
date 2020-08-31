78ea2d78a55dff95855bb5878c1779c4cbf1d705
Hemanshu Varma <hemanshuvarma916@gmail.com>
2020-08-31 23:06:00 +0530

    Add: Program5-ExplicitVariableType

---
+++ P5-ExplicitVariableType.kt
@@ -0,0 +1,11 @@
+//Execution starts from Main
+fun main() {
+
+    //Providing variable type explicitly
+    val myName: String = "Hemanshu Varma"
+    val age: Int = 5
+
+    //Prints type of variable, i.e., string, int, float etc
+    println(myName)
+    println(age)
+}
