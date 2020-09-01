28ad93528bb645e6df97670a8351cead2fe23422
Hemanshu Varma <hemanshuvarma916@gmail.com>
2020-09-01 22:35:18 +0530

    Add: P7-KotlinDataTypesStringAndChar

---
+++ P7-KotlinDataTypesStringAndChar.kt
@@ -0,0 +1,15 @@
+//Execution starts from Main
+fun main() {
+
+    val fullName: String = "Hemanshu Varma"
+
+    println(fullName.decapitalize()) //prints string with first letter converted to lowercase
+
+    println(fullName.toLowerCase()) // prints whole string in lowercase
+
+    val c: Char = 'H' // 16-bit unicode character.
+    //NOTE:
+    // 1. Allows only a single character, represented with single quotes
+    // 2. Allows escape characters( \n, \t, \\ and so on...)
+
+}
