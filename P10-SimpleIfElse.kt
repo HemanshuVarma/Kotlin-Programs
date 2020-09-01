1eb10ef7fae94afc7879cb9e9613d69ab7a43b70
Hemanshu Varma <hemanshuvarma916@gmail.com>
2020-09-01 23:27:30 +0530

    Add: P10-SimpleIfElse

---
+++ P10-SimpleIfElse.kt
@@ -0,0 +1,11 @@
+//Execution starts from Main
+fun main() {
+
+    val calories = 1500
+
+    if (calories > 2000) { //Checking this condition
+        println("You have reached today's goal") //Print this only when IF statement is true
+    } else { //Print this in any other scenario
+        println("You need to burn more calories")
+    }
+}
