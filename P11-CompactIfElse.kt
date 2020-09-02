e4fc32002cc22d60cef30e77b9a5a087355f1f0f
Hemanshu Varma <hemanshuvarma916@gmail.com>
2020-09-02 22:07:07 +0530

    Add: P11-CompactIfElse

---
+++ P11-CompactIfElse.kt
@@ -0,0 +1,11 @@
+//Execution starts from Main
+fun main() {
+
+    val calories = 1500
+
+    //Compact if/else
+    //Value of "Message" will be decided based upon result of if/else
+    val message = if (calories > 1500) "You reached goal" else "Need more workout"
+
+    println(message)
+}
