1a90e2d7c3f28a969387c6930e76ffded276eb72
Hemanshu Varma <hemanshuvarma916@gmail.com>
2020-10-03 23:09:04 +0530

    Add: P77-WhileLoop

---
+++ P77-WhileLoop.kt
@@ -0,0 +1,14 @@
+//Execution starts from Main
+fun main() {
+    //Kotlin While Loop
+
+    var x = 0
+
+    //While loop gets executed repeatedly until a condition is false
+    while (x <= 10) {
+        println(x) //Prints from 0 to 10
+        x++ //Inc logic check value is mandatory. Otherwise would be a infinite loop
+    }
+
+    println("Done")
+}
