4d7ce26ad81a5f8f522a9e75566a60515f61d297
Hemanshu Varma <hemanshuvarma916@gmail.com>
2020-10-22 23:14:34 +0530

    Add: P114-CallingJavaFromKotlin

---
+++ P114-CallingJavaFromKotlin/services/CustomerFacade.kt
@@ -0,0 +1,23 @@
+package services
+
+class CustomerFacade(customerService: CustomerService) {
+
+    init {
+
+        //When utilizing java methods data, type inference is important as they could vary(could be nullable)
+        //Java methods calls can be wrapped around requireNotNull() and other non-null operators like double bang(!!). Can be utilized based upon handling
+        val customerName = customerService.customerName("@VarmaHemanshu")
+
+        //Performing kotlin's operation on java response
+        customerName.name //Getter is invoked here
+
+        //Kotlin will not know the property type(mutable/immutable, nullable/non-nullable) until specified explicitly
+        val customersList = customerService.customersList()
+
+        //Performing kotlin specific functions
+        customersList.map {
+            //Mapping here
+        }
+        customersList.filterNotNull()
+    }
+}
