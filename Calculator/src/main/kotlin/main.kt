797ff84a45ac8073414416109e53089102c876c8
Hemanshu Varma <hemanshuvarma916@gmail.com>
2020-10-25 17:33:41 +0530

    Add: Taking user input for Calculator capstone project

--- Calculator/src/main/kotlin/main.kt
+++ Calculator/src/main/kotlin/main.kt
@@ -1,3 +1,16 @@
 fun main(vararg args: String) {
-    println("Hello Calculator")
+
+    //Welcome message to prompt user for input
+    println("Please enter arithmetic problem...")
+
+    //Reading user input
+    var input = readLine()
+
+    //Checking for user input. Shouldn't be null or empty(no blank spaces too!)
+    while (!input.isNullOrEmpty()) {
+        println("You entered: $input")
+        input = readLine()
+    }
+
+    println("GoodBye!")
 }
