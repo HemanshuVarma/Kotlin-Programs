24362e61baa90ebde75ff65022c6962b695d77b2
Hemanshu Varma <hemanshuvarma916@gmail.com>
2020-10-25 17:47:31 +0530

    Add: Core logic for Calculator

--- Calculator/src/main/kotlin/main.kt
+++ Calculator/src/main/kotlin/main.kt
@@ -1,3 +1,6 @@
+/**
+ * Calculator App
+ */
 fun main(vararg args: String) {
 
     //Welcome message to prompt user for input
@@ -8,7 +11,22 @@ fun main(vararg args: String) {
 
     //Checking for user input. Shouldn't be null or empty(no blank spaces too!)
     while (!input.isNullOrEmpty()) {
-        println("You entered: $input")
+        //Splitting input with a space
+        val values = input.split(' ')
+        val lhs = values[0]
+        val operator = values[1]
+        val rhs = values[2]
+
+        //Performing operation based on operator
+        when (operator) {
+            "+" -> println(lhs.toDouble() + rhs.toDouble())
+            "-" -> println(lhs.toDouble() - rhs.toDouble())
+            "*" -> println(lhs.toDouble() * rhs.toDouble())
+            "/" -> println(lhs.toDouble() / rhs.toDouble())
+            else -> throw IllegalArgumentException("Unknown operator: $operator")
+        }
+
+        //Continue taking input
         input = readLine()
     }
 
